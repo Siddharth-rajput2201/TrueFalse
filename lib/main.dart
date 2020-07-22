f533644c55d042022fe73e8cc37e7b5dc092e26f
Hush <rajputsiddharth18@gmail.com>
2020-07-22 13:03:52 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TrueFalse());
+}
+
+class TrueFalse extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade800,
+        body: SafeArea(
+          child: QuizPage(),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
